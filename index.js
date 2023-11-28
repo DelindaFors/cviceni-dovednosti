469d8656afd5ed11caed5b2c6b95e92f3bce856e
Daniela Ticha <daniela.ticha@post.cz>
2023-11-28 19:47:53 +0100

    4

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-console.log('funguju!')
+/*console.log('funguju!')*/
 
-const skill1 = prompt ("Zadej zkušenosti v HTML:")
+const skill1 = prompt ("Zadej zkušenosti v HTML:")  
 const skill2 = prompt ("Zadejte uzkušenosti v CSS:")
 const skill3 = prompt ("Zadejte uzkušenosti v JavaScriptu:")
 
 document.getElementById("skill1").querySelector(".skill__value").innerText = skill1 + " / 100";
 document.getElementById("skill2").querySelector(".skill__value").innerText = skill2 + " / 100";
 document.getElementById("skill3").querySelector(".skill__value").innerText = skill3 + " / 100";
+
