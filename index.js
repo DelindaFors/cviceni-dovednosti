ab2681440b8f731bf5c6d0034b91a60347909014
Daniela Ticha <daniela.ticha@post.cz>
2023-11-28 19:37:23 +0100

    neco

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 console.log('funguju!')
 
-const skill1 prompt ("Zadej zkušenosti v HTML:")
-const skill2 prompt ("Zadejte uzkušenosti v CSS:")
-const skill3 prompt ("Zadejte uzkušenosti v JavaScriptu:")
+const skill1 = prompt ("Zadej zkušenosti v HTML:")
+const skill2 = prompt ("Zadejte uzkušenosti v CSS:")
+const skill3 = prompt ("Zadejte uzkušenosti v JavaScriptu:")
 
-
+document.getElementById("skill1").querySelector(".skill__value").innerText = skill1 + " / 100";
+document.getElementById("skill2").querySelector(".skill__value").innerText = skill2 + " / 100";
+document.getElementById("skill3").querySelector(".skill__value").innerText = skill3 + " / 100";
