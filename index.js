9c1e4c9a76a4cdcb831e78e26d57558c31cdd67f
Daniela Ticha <daniela.ticha@post.cz>
2023-11-28 19:24:57 +0100

    Nove cviceni

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log('funguju!');
+console.log('funguju!')
+
+const skill1 prompt ("Zadej zkušenosti v HTML:")
+const skill2 prompt ("Zadejte uzkušenosti v CSS:")
+const skill3 prompt ("Zadejte uzkušenosti v JavaScriptu:")
+
+
